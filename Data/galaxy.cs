68921b548af27ab74cd585dd435eed12b5583b69
MaikolCode <maikol320434@gmail.com>
2021-03-19 13:58:56 -0500

    arranque

---
+++ Data/galaxy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tallerColecciones.Data
+{
+    public class galaxy
+    {
+        public string name { get; set; }
+        public int MegaLightYears { get; set; }
+    }
+}
